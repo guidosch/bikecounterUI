cbbcbd11ce9a03330373faf073122211adb361d3
guidosch <guido.schnider@gmail.com>
2021-12-25 14:04:37 +0100

    Changed products to counters

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,23 +1,16 @@
 import { Component } from '@angular/core';
 
-import { products } from '../products';
+import { counters } from '../counters';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-  products = products;
+  counters = counters;
 
   share() {
-    window.alert('The product has been shared!');
+    window.alert('The counter!');
   }
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
