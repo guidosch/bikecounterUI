0abf9473f6cb4b8d3341128b6ea3e013298e0be3
guidosch <guido.schnider@gmail.com>
2022-06-14 15:29:04 +0200

    Corrected bat. low warning to < 3.1 volt.

--- src/app/device-list/device-list.component.ts
+++ src/app/device-list/device-list.component.ts
@@ -35,10 +35,10 @@ export class DeviceListComponent {
   }
 
   calcWarnings(counter: Counter) {
-    //Devices start having problems sending msg. below 2.8 volt.
+    //Devices start having problems sending msg. below/equal ~3.0 volt.
     let errors: number = 0;
     if (counter.online){
-      if (counter.batteryVoltage && counter.batteryVoltage < 3.0){
+      if (counter.batteryVoltage && counter.batteryVoltage < 3.1){
         errors++;
       } else if (counter.humidity > 80) {
         errors++;
